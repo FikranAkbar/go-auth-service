40d975b90cb260af851a7dafb8ad4687fa3893f5
vendor-dev-fikran <fikran.vendor.pasti@pegadaian.co.id>
2025-11-27 19:50:11 +0700

    feat: create basic main.go file

---
+++ cmd/api/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "log"
+
+func main() {
+	// TODO: Initialize env as a struct
+	// TODO: Initialize database connection
+	// TODO: Initialize redis connection
+	// TODO: Initialize dependency injection
+	// TODO: Initialize router and routes
+	// TODO: Initialize http server
+	log.Println("API server is running...")
+}
